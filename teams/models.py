4d1d8c5ab776dd4ee094fcd4482a8f1dfd6c7b10
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-10-27 14:54:34 +0330

    added admin for team members

--- teams/models.py
+++ teams/models.py
@@ -1,16 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Team(models.Model):
-    user = models.OneToOneField(to='User', related_name='team')
+    user = models.OneToOneField(to=User, related_name='team')
     name = models.CharField(max_length=50)
 
 
 class TeamMember(models.Model):
-    team = models.ForeignKey(to='Team', related_name='members')
+    team = models.ForeignKey(to='Team', related_name='members', null=True)
     name = models.CharField(max_length=30)
     email = models.EmailField()
     entrance_year = models.IntegerField()
     university = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
 
