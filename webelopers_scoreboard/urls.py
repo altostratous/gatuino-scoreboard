c1e7c0511a29a10713d0927e5b17e0c060a89d06
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-10-27 17:58:23 +0330

    added judge request form

--- webelopers_scoreboard/urls.py
+++ webelopers_scoreboard/urls.py
@@ -19,5 +19,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html')),
+    url(r'^teams/', include('teams.urls', namespace='teams')),
     url(r'^', include('features.urls', namespace='features')),
 ]
