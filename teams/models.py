944018c4acca4c750c0af903aa37fddd87567b72
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-10-27 15:58:33 +0330

    some changes

--- teams/models.py
+++ teams/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 
 class Team(models.Model):
     user = models.OneToOneField(to=User, related_name='team')
     name = models.CharField(max_length=50)
+    is_official = models.BooleanField()
 
 
 class TeamMember(models.Model):
-    team = models.ForeignKey(to='Team', related_name='members', null=True)
+    team = models.ForeignKey(to='Team', related_name='members', null=True, on_delete=SET_NULL)
     name = models.CharField(max_length=30)
     email = models.EmailField()
     entrance_year = models.IntegerField()
