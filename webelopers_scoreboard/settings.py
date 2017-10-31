9bf756922f4cee0e36982a05bbb8c5f5bd62fe6d
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-10-31 11:27:16 +0330

    hid secret key for more security!

--- webelopers_scoreboard/settings.py
+++ webelopers_scoreboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wc^j#w$xdd3yzisj_6wi7c%p1)-*3w8cd2^*me*-$@$3u5#zl5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,6 +117,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Local settings
 local_settings_path = os.path.join(os.path.dirname(__file__), 'local_settings.py')
