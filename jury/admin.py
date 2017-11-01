3f41a8b06b5a829dbe0dbf0d389b2d875dc63364
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-11-01 18:56:23 +0330

    changed admin for judge request

--- jury/admin.py
+++ jury/admin.py
@@ -3,7 +3,16 @@ from solo.admin import SingletonModelAdmin
 
 from jury.models import Judge, JudgeRequest, JudgeRequestAssigment, Config
 
+
+class AssigneeInline(admin.TabularInline):
+    model = JudgeRequestAssigment
+
+
+class JudgeRequestAdmin(admin.ModelAdmin):
+    inlines = [AssigneeInline]
+
+
 admin.site.register(Judge)
-admin.site.register(JudgeRequest)
+admin.site.register(JudgeRequest, JudgeRequestAdmin)
 admin.site.register(JudgeRequestAssigment)
 admin.site.register(Config, SingletonModelAdmin)
