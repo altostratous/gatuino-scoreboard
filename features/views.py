677d2d3a023c07880cdd0ef9a455cbd67a01624f
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-11-01 11:30:47 +0330

    changed name to display_name

--- features/views.py
+++ features/views.py
@@ -17,11 +17,11 @@ class ScoreboardView(TemplateView):
         context = super().get_context_data(**kwargs)
         if Config.get_solo().day==1:
             context['headers'] = ['Team Name']+[feature.id for feature in Feature.objects.all()]+['Total Score']
-            context['standings']=sorted([[team.name]+[Attempt.objects.get(team=team,feature=feature).score if Attempt.objects.filter(team=team,feature=feature).exists() else '--' for feature in Feature.objects.filter(day=1)]+[functools.reduce(lambda x,y: x+y.score if y.score else x,Attempt.objects.filter(team=team),0)] for team in Team.objects.all()],key=itemgetter(len(context['headers'])-1),reverse=True)
+            context['standings']=sorted([[team.display_name]+[Attempt.objects.get(team=team,feature=feature).score if Attempt.objects.filter(team=team,feature=feature).exists() else '--' for feature in Feature.objects.filter(day=1)]+[functools.reduce(lambda x,y: x+y.score if y.score else x,Attempt.objects.filter(team=team),0)] for team in Team.objects.all()],key=itemgetter(len(context['headers'])-1),reverse=True)
 
         else:
             context['headers'] = ['Team Name','Day 1','Day 2','Total Score']
-            context['standings']=sorted([[team.name]+[functools.reduce(lambda x,y: x+y.score if y.feature.day==1 else x,Attempt.objects.filter(team=team),0)]+[functools.reduce(lambda x,y: x+y.score if y.feature.day==2 else x,Attempt.objects.filter(team=team),0)]+[functools.reduce(lambda x,y: x+y.score,Attempt.objects.filter(team=team),0)] for team in Team.objects.all()],key=itemgetter(3),reverse=True)
+            context['standings']=sorted([[team.display_name]+[functools.reduce(lambda x,y: x+y.score if y.feature.day==1 else x,Attempt.objects.filter(team=team),0)]+[functools.reduce(lambda x,y: x+y.score if y.feature.day==2 else x,Attempt.objects.filter(team=team),0)]+[functools.reduce(lambda x,y: x+y.score,Attempt.objects.filter(team=team),0)] for team in Team.objects.all()],key=itemgetter(3),reverse=True)
         return context
 
 class FeatureView(DetailView):
