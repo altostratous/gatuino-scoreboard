86125bf3fe65ed8f73e55bbbbd3dbed6dd8df3ea
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2019-05-01 14:56:20 +0000

    hardwar changes

--- webelopers_scoreboard/settings.py
+++ webelopers_scoreboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sag'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver', '127.0.0.1']
+ALLOWED_HOSTS = ['134.209.224.46']
 
 
 # Application definition
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'webelopers_scoreboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
